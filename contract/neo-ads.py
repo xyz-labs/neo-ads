3add868aa6b51f6d154364a1a844f93d2c5f08d6
Mirren King-Smith <mirren@live.com.au>
2018-08-25 01:41:42 +0900

    Added basic DeletePublication contract method and checks

--- contract/neo-ads.py
+++ contract/neo-ads.py
@@ -22,6 +22,13 @@ def Main(operation, args):
                 return [False, '4 arguments required']
 
             return CreatePublication(args)
+
+        elif operation == 'deletePublication':
+            if nargs != 2:
+                print('Required arguments: [sender] [name]')
+                return [False, '2 arguments required']
+
+            return DeletePublication(args)  
         
         elif operation == 'viewUserPublications':
 
@@ -61,8 +68,17 @@ def CreatePublication(args):
     context = GetContext()
 
     publications_key = concat('publications', sender)      # Publications by user
+    publication_key = concat(publications_key, sha1(name)) # Publication details - sha1 to prevent malicious input
 
     publications = Get(context, publications_key)
+    publication = Get(context, publication_key)
+    
+    # If publication already exists check if it is active/deleted
+    if publication:
+        publication = Deserialize(publication)
+        if publication[4]:
+            print('Publication name currently active')
+            return [False, 'Active publication name']
     
     # Check if user has publications already
     if publications:
@@ -71,15 +87,47 @@ def CreatePublication(args):
         publications = []
 
     first_date = GetTime() + (SECONDS_IN_DAY - GetTime() % SECONDS_IN_DAY)
+    is_active = True
 
-    new_publication = [name, url, category, first_date]
+    new_publication = [name, url, category, first_date, is_active]
     publications.append(name)
-
-    publication_key = concat(publications_key, sha1(name)) # Publication details - sha1 to prevent malicious input
 
     Put(context, publication_key, Serialize(new_publication))
     Put(context, publications_key, Serialize(publications))
 
+    return [True, '']
+
+def DeletePublication(args):
+    sender = args[0]
+    name = args[1]
+
+    if not CheckWitness(sender):
+        print('Account owner must be sender')
+        return [False]
+
+    context = GetContext()
+
+    publications_key = concat('publications', sender)
+    publication_key = concat(publications_key, sha1(name))
+
+    publication = Get(context, publication_key)
+    if len(publication) == 0:
+        print('Publication does not exist')
+        return [False, 'Publication does not exist']
+
+    publication = Deserialize(publication)
+    if not publication[4]:
+        print('Publication has already been deleted')
+        return [False, 'Publication has already been deleted']
+
+    """
+    Check for active bids etc here - revisit if time (OOS)
+    """
+
+    publication[4] = False
+    publication = Serialize(publication)
+
+    Put(context, publication_key, publication)
     return [True, '']
 
 def ViewUserPublications(args):
