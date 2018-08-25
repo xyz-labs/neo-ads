e1832bf4be433acceab6b5ed095f901c5b07d923
Mirren King-Smith <mirren@live.com.au>
2018-08-25 17:12:38 +0900

    Add header html

--- website/src/components/App/App.js
+++ website/src/components/App/App.js
@@ -20,7 +20,7 @@ import Tags from '../../containers/Tags/Tags';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="body">
         <header>
           <Header/>
         </header>
--- website/src/containers/Header/Header.js
+++ website/src/containers/Header/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Immutable from 'immutable';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
 import './Header.css';
 import { checkNeoLinkStatus } from '../../reducers/neolink';
+
+import Logo from '../../images/logo.svg'
 
 export class Header extends Component {
   constructor(props, context) {
@@ -21,8 +22,22 @@ export class Header extends Component {
 
   render() {
     return (
-      <div className="Header">
-        Header
+      <div className="header">
+        <div>
+          <div className="row w-row">
+            <div className="column w-col w-col-6">
+                <Link to="/home" className="link-block w-inline-block">
+                  <img src={Logo} width="83" className="logo-img"/>
+               </Link>
+            </div>
+            <div className="column-2 w-col w-col-6">
+              <div className="text-block">AyM4...6Su3</div>
+                <Link to="/account" className="button-secondary w-button">
+                  Account
+                </Link>
+              </div>
+          </div>
+        </div>
       </div>
     );
   }
