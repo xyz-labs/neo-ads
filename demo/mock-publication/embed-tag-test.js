a23223759a051d632111e94f458c59d87587814d
Tsering Redmond <tsering@blocksports.com>
2018-08-26 00:18:32 +0900

    Fixed ad embed tag bug with base 64 img

--- demo/mock-publication/embed-tag-test.js
+++ demo/mock-publication/embed-tag-test.js
@@ -11,15 +11,12 @@ jQuery(document).ready(function ($) {
 var id = 'xxx'
 
 //variables must be queired from neon-js
-var filler = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
 var adUrl = 'https://www.blocksports.com';
 var img1Url = 'https://i.imgur.com/HpFfvm5.png';
 var img2Url = 'https://i.imgur.com/tjwcJjK.png';
 
 
 //jquery to replace values
-$('.na-img1').attr('src', filler);
-$('.na-img2').attr('src', filler);
 $('.na-img1').attr('src', img1Url);
 $('.na-img2').attr('src', img2Url);
 $('.na-div').attr('href', adUrl);
@@ -29,11 +26,11 @@ $('.na-div').attr('href', adUrl);
 
 //Image 1 (728x90)
 <a class="na-div" style="width:728px;height:90px;" href="#">
-  <img class="na-img1" src="#">
+  <img class="na-img1" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">
 </a>
 
 
 //Image 2 (300x250)
 <a class="na-div" style="width:300px;height:250px;" href="#">
-  <img class="na-img2" src="#">
+  <img class="na-img2" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">
 </a>
